8d661b4332fde63624131d027a7d20f4a7c33bb5
lizhengtian <562265055@qq.com>
2017-03-22 10:02:50 +0800

    first commit

---
+++ test_1/test_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_1
+//
+//  Created by 小咖Lee on 2017/3/22.
+//  Copyright © 2017年 小咖Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_1/test_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_1
+//
+//  Created by 小咖Lee on 2017/3/22.
+//  Copyright © 2017年 小咖Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
